79c39c3c15007e31f85fe6ea03fb6e9d25e7628e
devmpatel2005 <devmpatel2005@gmail.com>
2025-08-26 21:17:37 -0400

    Working backend with adjusted model

---
+++ backend/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel
+
+modelPath = "../model/medicalCostModel2.pkl"
+model = joblib.load(modelPath)
+
+app = FastAPI()
+
+class HealthData(BaseModel):
+    age: int
+    sex: str
+    bmi: float
+    children: int
+    smoker: str
+    region: str
+
+def makeFeatures(data: HealthData):
+    sex_map = {'male': 0, 'female': 1}
+    smoker_map = {'no': 0, 'yes': 1}
+    reigon_map = {'northeast': 0, 'northwest': 1, 'southeast': 2, 'southwest': 3}
+
+    features = [
+        data.age,
+        sex_map[data.sex.lower()],
+        data.bmi,
+        data.children,
+        smoker_map[data.smoker.lower()],
+        reigon_map[data.region.lower()]
+    ]
+
+    return features
+
+
+@app.post("/predict")
+def predict(data: HealthData):
+    input_features = [makeFeatures(data)]  # wrap in list → single-row input
+    prediction = model.predict(input_features)[0]
+    return {"predicted_charges": float(prediction)}
