8fc3177fd266de1dc979b407a5d27d7324380975
devmpatel2005 <devmpatel2005@gmail.com>
2025-09-01 17:23:20 -0400

    Create form to get necesarry information to predict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,54 @@ function App() {
   return (
     <div className="App">
       <h1>Medical Cost Estimator</h1>
+      <form>
+
+        <div id='Age'>
+          <label>Age: </label>
+          <input type='number' name='age' min='0'/>
+        </div>
+
+        <div id='Sex'>
+          <label>Sex: </label>
+          <select name='sexDropdown'>
+            <option value='Choose Below'></option>
+            <option value='male'>Male</option>
+            <option value='female'>Female</option>
+          </select>
+        </div>
+
+        <div id='BMI'>
+          <label>BMI: </label>
+          <input type='number' step='0.1' name='bmi' min='0'/>
+        </div>
+
+        <div id='children'>
+          <label>Children: </label>
+          <input type='number' name='children' min='0'/>
+        </div>
+
+        <div id='smoker'>
+          <label>Smoker: </label>
+          <select name='smokerDropdown'>
+            <option value='Choose Below'></option>
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </select>
+        </div>
+
+        <div id="region">
+          <label>Region: </label>
+          <select name='regionDropdown'>
+            <option value='Choose Below'></option>
+            <option value='northeast'>Northeast</option>
+            <option value='northeast'>Northwest</option>
+            <option value='northeast'>Southeast</option>
+            <option value='northeast'>Southwest</option>
+          </select>
+        </div>
+        <button type='submit'>Predict</button>
+
+      </form>
     </div>
   );
 }
