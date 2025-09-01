02af529a89a0672686bead1f05afbde760eda114
devmpatel2005 <devmpatel2005@gmail.com>
2025-09-01 19:01:10 -0400

    Form properly works and catches data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,19 +10,35 @@ function App() {
   const [smoker, setSmoker] = useState('');
   const [region, setRegion] = useState('');
 
+  const handleSubmit = (submit) => {
+    submit.preventDefault();
+    const formData = { age, sex, bmi, children, smoker, region };
+    console.log("Form data:", formData);
+  };
+
   return (
     <div className="App">
       <h1>Medical Cost Estimator</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
 
         <div id='Age'>
           <label>Age: </label>
-          <input type='number' name='age' min='0'/>
+          <input 
+          type='number' 
+          name='age' 
+          min='0' 
+          value={age} 
+          onChange={(submit) => setAge(submit.target.value)}
+          />
         </div>
 
         <div id='Sex'>
           <label>Sex: </label>
-          <select name='sexDropdown'>
+          <select 
+          name='sexDropdown'
+          value={sex}
+          onChange={(submit) => setSex(submit.target.value)}
+          >
             <option value='Choose Below'></option>
             <option value='male'>Male</option>
             <option value='female'>Female</option>
@@ -31,17 +47,34 @@ function App() {
 
         <div id='BMI'>
           <label>BMI: </label>
-          <input type='number' step='0.1' name='bmi' min='0'/>
+          <input 
+          type='number' 
+          step='0.1' 
+          name='bmi' 
+          min='0'
+          value={bmi}
+          onChange={(submit) => setBmi(submit.target.value)}
+          />
         </div>
 
         <div id='children'>
           <label>Children: </label>
-          <input type='number' name='children' min='0'/>
+          <input 
+          type='number' 
+          name='children' 
+          min='0'
+          value={children}
+          onChange={(submit) => setChildren(submit.target.value)}
+          />
         </div>
 
         <div id='smoker'>
           <label>Smoker: </label>
-          <select name='smokerDropdown'>
+          <select 
+          name='smokerDropdown'
+          value={sex}
+          onChange={(submit) => setSmoker(submit.target.value)}
+          >
             <option value='Choose Below'></option>
             <option value='yes'>Yes</option>
             <option value='no'>No</option>
@@ -50,7 +83,11 @@ function App() {
 
         <div id="region">
           <label>Region: </label>
-          <select name='regionDropdown'>
+          <select 
+          name='regionDropdown'
+          value={region}
+          onChange={(submit) => setRegion(submit.target.value)}
+          >
             <option value='Choose Below'></option>
             <option value='northeast'>Northeast</option>
             <option value='northeast'>Northwest</option>
