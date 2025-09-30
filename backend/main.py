fc3d3df855a24ab313c8266ab27d60a66f3347e9
devmpatel2005 <devmpatel2005@gmail.com>
2025-09-30 00:00:42 -0400

    Fixed backend

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["https://medicalchargesesitmator.onrender.com"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
