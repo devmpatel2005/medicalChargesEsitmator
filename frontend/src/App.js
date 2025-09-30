ffdcf3223ebc835c023a9baf9d3a8f37180666d3
devmpatel2005 <devmpatel2005@gmail.com>
2025-09-29 23:56:17 -0400

    Connect to render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -105,7 +105,7 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://medicalchargesesitmator.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
